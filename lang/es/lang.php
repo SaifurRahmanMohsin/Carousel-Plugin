3efe85567245e0fb4705fba2d8b372a1b362a22a
merodeador <merodeador@users.noreply.github.com>
2015-12-18 21:15:37 +0100

    Add spanish language.

---
+++ lang/es/lang.php
@@ -0,0 +1,32 @@
+<?php
+return [
+  'plugin' => [
+    'name'                => 'Bootstrap Carousel',
+    'description'         => 'Crea presentaciones de imágenes tipo carrusel.',
+    'tab'                 => 'Presentaciones',
+    'permission_label'    => 'Administrar presentaciones'
+  ],
+  'carousels' => [
+    'name'                => 'Nombre',
+    'plural'              => 'Presentaciones',
+    'new_carousel'        => 'Nueva presentación',
+    'create_carousels'    => 'Crear presentaciones',
+    'update_carousels'    => 'Actualizar presentaciones',
+    'manage_carousels'    => 'Administrar presentaciones',
+    'preview_carousels'   => 'Previsualizar presentaciones',
+    'delete_confirm'      => 'Seguro que desea borrar esta presentación?',
+    'return_to_carousels' => 'Volver a la lista de presentaciones',
+  ],
+  'settings' => [
+    'name'        => 'Presentación carrusel',
+    'description' => 'Añadir presentación a la página.',
+    'choice'      => 'Seleccione la presentación que desea mostrar.',
+  ],
+  'model' => [
+    'name'    => 'Nombre',
+    'photos'  => 'Imágenes',
+    'id'      => 'ID',
+    'created' => 'Creado',
+    'updated' => 'Actualizado',
+  ],
+];
