afc976b52a427b9ce45842314faabc11c5a4b04c
Syed-sulaimaan <sysulaimaan@gmail.com>
2021-10-17 02:19:29 +0530

    Added Hindi Language

---
+++ lang/hi/lang.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+  'plugin' => [
+      'name' => 'Bootstrap Carousel',
+      'description' => 'Create responsive carousels.',
+      'tab' => 'Carousel'
+  ],
+  'carousels' => [
+      'name' => 'Name',
+      'plural' => 'Carousels',
+      'new_carousel' => 'New Carousel',
+      'create_carousels' => 'Create Carousels',
+      'update_carousels' => 'Update Carousels',
+      'manage_carousels' => 'Manage Carousels',
+      'preview_carousels' => 'Preview Carousels',
+      'delete_confirm' => 'Do you really want to delete this carousel?',
+      'return_to_carousels' => 'Return to carousels list',
+  ],
+  'settings' => [
+      'name' => 'Carousel',
+      'description' => 'Add a carousel to the page.',
+      'choice' => 'Choose the carousel that you want to display.',
+  ],
+  'preferences' => [
+    'show_carousels_in_nav_label' => 'Display the carousels in navigation',
+    'show_carousels_in_nav_comment' => 'Enabling this makes the carousels appear in the navigation menu instead of the settings page.',
+  ],
+  'model' => [
+      'name' => 'Name',
+      'photos' => 'Photos',
+      'id' => 'ID',
+      'created' => 'Created at',
+      'updated' => 'Updated at',
+  ],
+  'permissions' => [
+    'manage_carousels' => 'Manage the carousels',
+  ]
+];
